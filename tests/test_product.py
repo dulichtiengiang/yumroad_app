b322a86377570e528743d51bf2045df3250b5ac7
victhannguyen <victhangnguyen@gmail.com>
2021-05-27 18:54:55 +0700

    update 2

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,7 +1,15 @@
 import pytest
-
+from flask import url_for
 from yumroad.extensions import db
 from yumroad.models import Product
+
+
+@pytest.fixture
+def sample_book():
+    book = Product(name="Book of Dev", description="Learning for Dev")
+    db.session.add(book)
+    db.session.commit()
+    return book
 
 def test_product_creation(client, init_database):
     assert Product.query.count() == 0
@@ -15,3 +23,25 @@ def test_name_validation(client, init_database):
     with pytest.raises(ValueError):
         Product(name="   a", description="invalid book")
 
+def test_index_page(client, init_database, sample_book):
+    # client.get('/product') #blueprint('/product')
+    response = client.get(url_for('products.index'))
+    assert response.status_code == 200
+    assert 'Yumroad' in str(response.data)
+    assert sample_book.name in str(response.data)
+
+    expected_link = url_for('products.details', product_id=sample_book.id)
+    assert expected_link in str(response.data)
+
+def test_detail_page(client, init_database, sample_book):
+    # client.get('/product/1')
+    response = client.get(url_for('products.details', product_id=sample_book.id))
+    assert response.status_code == 200
+    assert 'Yumroad' in str(response.data) 
+    assert 'Purchase coming soon' in str(response.data) 
+
+def test_not_found_page(client, init_database):
+    # client.get('/product/1')
+    response = client.get(url_for('products.details', product_id=1))
+    assert response.status_code == 404
+    assert url_for('products.index') in str(response.data) 
--- yumroad/blueprints/product.py
+++ yumroad/blueprints/product.py
@@ -2,18 +2,18 @@ from flask import Blueprint, render_template, abort
 
 from yumroad.models import Product
 
-product = Blueprint('product', __name__)
+product = Blueprint('products', __name__)
 
 @product.route('/')
 def index():
     # all_products = Product.query.all()
     # return all_products
-    return ('This is Index Page')
+    product_all = Product.query.all()
+    return render_template('/products/index.html', product=product_all)
 
 @product.route('/<int:product_id>')
-def detail(product_id):
+def details(product_id):
     product = Product.query.get_or_404(product_id)
-
     return render_template('/products/details.html', product=product)
 
 @product.errorhandler(404)
