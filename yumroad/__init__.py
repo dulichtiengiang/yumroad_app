2e4e287ae3c59e9252a116c235196aac968d0937
victhangnguyen <victhangnguyen@gmail.com>
2021-05-28 18:55:42 +0700

    udpate 28 05 2021

--- yumroad/__init__.py
+++ yumroad/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from yumroad.config import configuration
 from yumroad.extensions import db
 
-from yumroad.blueprints.product import product as bp_product
+from yumroad.blueprints.products import products as bp_products
 
 def create_app(environment_name='dev'):
     app = Flask(__name__)
@@ -11,6 +11,6 @@ def create_app(environment_name='dev'):
     app.config.from_object(configuration[environment_name])
     db .init_app(app)
 
-    app.register_blueprint(bp_product, url_prefix='/product')
+    app.register_blueprint(bp_products, url_prefix='/product')
 
     return app
--- yumroad/blueprints/products.py
+++ yumroad/blueprints/products.py
@@ -2,20 +2,20 @@ from flask import Blueprint, render_template, abort
 
 from yumroad.models import Product
 
-product = Blueprint('products', __name__)
+products = Blueprint('products', __name__)
 
-@product.route('/')
+@products.route('/')
 def index():
     # all_products = Product.query.all()
     # return all_products
     product_all = Product.query.all()
     return render_template('/products/index.html', product=product_all)
 
-@product.route('/<int:product_id>')
+@products.route('/<int:product_id>')
 def details(product_id):
     product = Product.query.get_or_404(product_id)
     return render_template('/products/details.html', product=product)
 
-@product.errorhandler(404)
+@products.errorhandler(404)
 def not_found(exception):
     return render_template('products/404.html'), 404
